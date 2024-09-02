9640879cf6ce69995a2eece638b3c2bd0812679c
aliwehbi5 <ali_h_wehby@hotmail.com>
2024-09-02 12:38:50 +0300

    Update Vue configuration file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+import { defineConfig } from "@vue/cli-service";
+
+export default defineConfig({
   transpileDependencies: true,
   publicPath: "/REST-Countries-API-with-color-theme-switcher",
 });
