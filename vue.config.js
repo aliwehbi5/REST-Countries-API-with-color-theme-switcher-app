8329981f765254f4a8f83d6be70c0300bda28bce
aliwehbi5 <ali_h_wehby@hotmail.com>
2024-09-02 12:07:59 +0300

    first commit

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: "/REST-Countries-API-with-color-theme-switcher",
+});
